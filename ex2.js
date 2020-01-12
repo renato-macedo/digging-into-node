3fb8c2a3a7e78a7ab3257ca1ed8add5d53348113
Renato Macedo <renatomaceneto@gmail.com>
2020-01-12 18:15:50 -0300

    finish ex2.js

--- ex2.js
+++ ex2.js
@@ -3,13 +3,17 @@
 const util = require("util");
 const path = require("path");
 const fs = require("fs");
+const { Transform } = require('stream');
+const zlib = require('zlib');
 
 const args = require("minimist")(process.argv.slice(2), {
-  boolean: ["help", "in"],
+  boolean: ["help", "in", "out", "compress", "uncompress"],
   string: ["file"]
 });
 
-const BASE_PATH = path.resolve(process.env.BASE_PATH || __dirname);
+var BASE_PATH = path.resolve(process.env.BASE_PATH || __dirname);
+
+var OUTFILE = path.join(BASE_PATH, "out.txt");
 
 if (args.help) {
   
@@ -33,8 +37,36 @@ if (args.help) {
 // *****************************
 
 function processFile(inStream) {
-  const outStream = inStream;
-  const targetStream = process.stdout;
+  let outStream = inStream;
+
+  if (args.uncompress) {
+    let gunzipStream = zlib.createGunzip();
+    outStream = outStream.pipe(gunzipStream)
+  }
+
+  const upperStream = new Transform({
+    transform(chunk, enc, callback) {
+      this.push(chunk.toString().toUpperCase())
+      callback()
+    }
+  })
+
+  outStream = outStream.pipe(upperStream);
+
+  if (args.compress) {
+
+    let gzipStream = zlib.createGzip();
+    outStream = outStream.pipe(gzipStream);
+    OUTFILE = `${OUTFILE}.gz`;
+  }
+
+  let targetStream;
+  
+  if (args.out) {
+    targetStream = process.stdout; 
+  } else {
+    targetStream = fs.createWriteStream(OUTFILE);
+  }
 
   outStream.pipe(targetStream);
 }
@@ -53,4 +85,8 @@ function printHelp() {
   console.log("--help                   print this help\n");
   console.log("--file={FILENAME}        process the file");
   console.log("--in, -                  process stdin");
+  console.log("--out                    print to stdout");
+  console.log("--compress               gzip the output");
+  console.log("--uncompress             un-gzip the input");
+  console.log("")
 }
